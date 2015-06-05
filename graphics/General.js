2c9fad63702a18947c25956584a841e20283f33b
megalm1 <megalm1@gmail.com>
2015-06-05 21:29:50 +0300

    Fix JS and jQuery
    all kinds of fixes in client side.

--- graphics/General.js
+++ graphics/General.js
@@ -4,7 +4,7 @@
     $("#main_search").stop(true).animate({ "margin-top": Math.max(($(window).height() - $("#main_search").outerHeight()) / 2, 0) }, 1000);
     $("#rightColumn").stop(true).animate({ "margin-top": Math.max(($(window).height() - $("#rightColumn").outerHeight()) / 2, 0) }, 1000);
 }
-function appear(list){
+function appear(list) {
     list
     .not("br, script")
     .each(function (index, elem) {
@@ -39,26 +39,26 @@ function slide(navigation_id, pad_hover, timePerAnim, waitPerAnim) {
             $(navigation_id).animate({ "paddingTop": "-=" + (menuScroll - menuScrollTop - limit) }, 1000, "elasout"); //bounceout?
     });
 }*/
-function checkPatternValid(patternStr){
+function checkPatternValid(patternStr) {
     // return true if pattern is valid and not empty(!)
     // use $.trim(patternStr)=='' to check if patternStr is empty.
     return !/[^אבגדהוזחטיכלמנסעפצקרשתךםןףץ ?]/.test(patternStr);
 }
-function isFormValid(self){
-    inputs=[self.definition, self.answer, self.pattern]
-    out=true;
+function isFormValid(self) {
+    inputs = [self.definition, self.answer, self.pattern]
+    out = true;
     hasError = false;
     $("#errorMessage").empty();
-    for(i=0; i<inputs.length; i++){
+    for (i = 0; i < inputs.length; i++) {
         $(inputs[i]).val($.trim($(inputs[i]).val()));
-        if(inputs[i]) {
-            if($(inputs[i]).val()==''  ||
-                (inputs[i]==self.pattern &&
+        if (inputs[i]) {
+            if ($(inputs[i]).val() == '' ||
+                (inputs[i] == self.pattern &&
                     !checkPatternValid($(inputs[i]).val()))) {
-                out=false;
+                out = false;
                 $(inputs[i]).addClass("angryPeleg");
                 if (!hasError) {
-                    if ($(inputs[i]).val()=='') {
+                    if ($(inputs[i]).val() == '') {
                         if (i == 0) {
                             $("#errorMessage").append("אנא הכנס הגדרה")
                             hasError = true;
@@ -68,9 +68,9 @@ function isFormValid(self){
                             hasError = true;
                         }
                     }
-                    if ((inputs[i]==self.pattern &&
+                    if ((inputs[i] == self.pattern &&
                         !checkPatternValid($(inputs[i]).val()))) {
-                         $("#errorMessage").append("אנא הכנס תבנית חוקית")
+                        $("#errorMessage").append("אנא הכנס תבנית חוקית")
                         hasError = true;
                     }
                 }
@@ -94,19 +94,20 @@ function closePopup() {
     });
 }
 
-function showPopup(content, focusIndex) {
+function showPopup(content, onPopupReady) {
     closePopup();
-    // focusIndex is a default parameter that indicates which textbox should be focused
-    focusIndex = typeof focusIndex !== 'undefined' ? focusIndex : 0;
     $("#currentPopup").append("<div id='blackblock'></div>");
     $("#blackblock").click(closePopup).fadeIn(500, function () {
         $("#currentPopup").append("<div id='popupContentHolder'><img src='graphics/fancy_close.png' id='small_x' alt='' onclick='closePopup()' /></div>");
         $("#popupContentHolder").append(content);
         fixCSSIssues();
-	$("#popupContentHolder").fadeIn(500, function () {
+        $("#popupContentHolder").css("top", "-=30").fadeIn({ queue: false, duration: 500 }).animate({ top: "+=30" }, 500, function () {
             $(this).children().fadeIn(500);
         });
-        $("#currentPopup input:text").eq(focusIndex).focus();
+        if (typeof onPopupReady !== 'undefined')
+            onPopupReady.call($("#popupContentHolder"));
+        else
+            $("#popupContentHolder input:first").focus();
     });
 }
 function setPopups() {
@@ -141,7 +142,7 @@ function searchDoneAppear(data) {
     searchDone(data);
     appear($("#results").children());
 }
-function submitAForm (addr, values, doneFunc) {
+function submitAForm(addr, values, doneFunc) {
     $.get(addr, values)
     .done(doneFunc)
     .fail(ajaxFail);
@@ -149,21 +150,22 @@ function submitAForm (addr, values, doneFunc) {
 function expirementWithForms() {
     $("#main_search form").submit(function (e) {
         e.preventDefault();
-        if(isFormValid(this)) {
+        if (isFormValid(this)) {
             $("#results").empty().append('<img src="graphics/spinner.gif" />');
+            fixCSSIssues();
             appear($("#results").children());
             submitAForm($(this).attr("action"), $(this).serialize(), searchDoneAppear);
         }
     });
     $(document).on("submit", "#add_defi", function (e) {
         e.preventDefault();
-        if(!isFormValid(this)) return;
+        if (!isFormValid(this)) return;
         $("#popupContentHolder").fadeOut(500);
         submitAForm($(this).attr("action"), $(this).serialize(), function (data) {
             $("#popupContentHolder").fadeIn({
                 duration: 500,
-                start: function(){
-                    $(this).empty().html("<h3>"+data+"</h3>");
+                start: function () {
+                    $(this).empty().html("<h3>" + data + "</h3>");
                 }
             });
         });
@@ -174,12 +176,15 @@ $(document).ready(function () {
     slide("#slidingNavigation", 17, 900, 150);
     setPopups();
     expirementWithForms();
-    $(document).on("focus","input",function () {
+    $(document).on("focus", "input", function () {
         $(this).removeClass("angryPeleg");
+    });
+    $(document).keyup(function (e) {
+        if (e.keyCode == 27) closePopup(); // escape key maps to keycode `27`
     });
 });
 
-window.onload = function(){ 
+window.onload = function () {
     fixCSSIssues();
     appear($("#main_search form").children());
 };
