8b70dc46fc88114de3a0b898e9b71ec70e64e7da
megalm1 <megalm1@gmail.com>
2015-08-18 17:02:33 +0300

    popup fix
    not sure why it was like that from the beginning..

--- graphics/javascript/Popups.js
+++ graphics/javascript/Popups.js
@@ -7,9 +7,9 @@ function closePopup() {
 function showPopup(content, onPopupReady) {
     closePopup();
     $("#currentPopup").append("<div id='blackblock'></div>");
-    $("#blackblock").click(closePopup).fadeIn(500, function () {
         $("#currentPopup").append("<div id='popupContentHolder'><img src='graphics/fancy_close.png' id='small_x' width='30px' alt='' onclick='closePopup()' /></div>");
         $("#popupContentHolder").append(content);
+    $("#blackblock").click(closePopup).fadeIn(500, function () {
         $("#popupContentHolder").css("right", ($(window).width() - $("#popupContentHolder").outerWidth()) / 2);
         $("#popupContentHolder").css("top", ($(window).height() - $("#popupContentHolder").outerHeight()) / 2);
         $("#popupContentHolder").css("top", "-=30").fadeIn({ queue: false, duration: 500 }).animate({ top: "+=30" }, 500, function () {
